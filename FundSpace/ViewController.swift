8744cbadf1b37153a4f734fcc14668a080fb4274
admin <admin@admins-Mac.local>
2019-04-07 22:17:33 +0200

    Initial Commit

---
+++ FundSpace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FundSpace
+//
+//  Created by admin on 4/7/19.
+//  Copyright © 2019 Zhang Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
