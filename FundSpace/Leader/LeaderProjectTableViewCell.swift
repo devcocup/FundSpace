2e308f3b4c45cb18a4389cb4a9e7dd3931a0acf7
typescript59 <typescript59@gmail.com>
2019-09-11 02:32:35 +0800

    [update] Implement the Leader Project Over View Screen.

---
+++ FundSpace/Leader/LeaderProjectTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaderProjectTableViewCell.swift
+//  FundSpace
+//
+//  Created by admin on 9/9/19.
+//  Copyright © 2019 Zhang Hui. All rights reserved.
+//
+
+import UIKit
+
+class LeaderProjectTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
